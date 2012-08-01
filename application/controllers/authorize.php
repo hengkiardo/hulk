19a7040d981cde6f503c69bbc50a036d395bbf9a
Jepri Sinaga <therexxar@gmail.com>
2012-08-01 17:33:14 +0700

    test controller
    gw test di authorize ya ki,
    bissa di remove di function index kalo gak perlu

--- application/controllers/authorize.php
+++ application/controllers/authorize.php
@@ -13,19 +13,29 @@ class Authorize extends CI_Controller {
 	{
 		
 		if(isset($_GET['code']) && $_GET['code'] != '') {
-			
+			$data = array();
+
 			$auth_response = $this->instagram_api->authorize($_GET['code']);
 			//var_dump($auth_response);
 			
-			// Set up session variables containing some useful Instagram data
-			$this->session->set_userdata('instagram-token', $auth_response->access_token);
-			$this->session->set_userdata('instagram-username', $auth_response->user->username);
-			$this->session->set_userdata('instagram-profile-picture', $auth_response->user->profile_picture);
-			$this->session->set_userdata('instagram-user-id', $auth_response->user->id);
-			$this->session->set_userdata('instagram-full-name', $auth_response->user->full_name);
+			if(isset($auth_response) && !empty($auth_response)){
+				// Set up session variables containing some useful Instagram data
+				$this->session->set_userdata('instagram-token', $auth_response->access_token);
+				$this->session->set_userdata('instagram-username', $auth_response->user->username);
+				$this->session->set_userdata('instagram-profile-picture', $auth_response->user->profile_picture);
+				$this->session->set_userdata('instagram-user-id', $auth_response->user->id);
+				$this->session->set_userdata('instagram-full-name', $auth_response->user->full_name);
+
+				$data['id'] = $auth_response->user->id;
+
+				if(isset($data['id'])){
+					header('Location:'. base_url().'home/');
+				}	
+			}else{
+				header('Location:'. base_url().'home/');
+			}
 			
-			redirect('/home','Location', 300);
-			
+
 		} else {
 			
 			// There was no GET variable so redirect back to the homepage
